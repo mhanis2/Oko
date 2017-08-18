49839db443b341aeb83d11cce5a545679ef5a15d
mhanis2 <marcel.hanis@itvkurze.sk>
2017-08-18 23:11:01 +0200

    MH

--- src/factorycards/CardsFactory.java
+++ src/factorycards/CardsFactory.java
@@ -6,197 +6,204 @@ import model.card.Value;
 
 public interface CardsFactory {
 
+	/**
+	 * Create card according to color and value.
+	 * 
+	 * @param color
+	 * @param label
+	 * @return Card with set color and value
+	 */
 	public Card createCard(Color color, Value label);
-	/**
-	 * Create card Clubs Seven.
-	 * @return card Clubs Seven
-	 */
-	public Card createCardClubsSeven();
-
-	/**
-	 * Create card Clubs Eight.
-	 * @return card Clubs Eight
-	 */
-	public Card createCardClubsEight();
-
-	/**
-	 * Create card Clubs Nine.
-	 * @return card Clubs Nine
-	 */
-	public Card createCardClubsNine();
-
-	/**
-	 * Create card Clubs Ten.
-	 * @return card Clubs Ten
-	 */
-	public Card createCardClubsTen();
-
-	/**
-	 * Create card Clubs Jack.
-	 * @return card Clubs Jack
-	 */
-	public Card createCardClubsJack();
-
-	/**
-	 * Create card Clubs Queen.
-	 * @return card Clubs Queen
-	 */
-	public Card createCardClubsQueen();
-
-	/**
-	 * Create card Clubs King.
-	 * @return card Clubs King
-	 */
-	public Card createCardClubsKing();
-
-	/**
-	 * Create card Clubs Ace.
-	 * @return card Clubs Ace
-	 */
-	public Card createCardClubsAce();
-
-	/**
-	 * Create card Clubs Seven.
-	 * @return card Clubs Seven
-	 */
-	public Card createCardDiamondsSeven();
-
-	/**
-	 * Create card Diamonds Eight.
-	 * @return card Diamonds Eight
-	 */
-	public Card createCardDiamondsEight();
-
-	/**
-	 * Create card Diamonds Nine.
-	 * @return card Diamonds Nine
-	 */
-	public Card createCardDiamondsNine();
-
-	/**
-	 * Create card Diamonds Ten.
-	 * @return card Diamonds Ten
-	 */
-	public Card createCardDiamondsTen();
-
-	/**
-	 * Create card Diamonds Jack.
-	 * @return card Diamonds Jack
-	 */
-	public Card createCardDiamondsJack();
-
-	/**
-	 * Create card Diamonds Queen.
-	 * @return card Diamonds Queen
-	 */
-	public Card createCardDiamondsQueen();
-
-	/**
-	 * Create card Diamonds King.
-	 * @return card Diamonds King
-	 */
-	public Card createCardDiamondsKing();
-
-	/**
-	 * Create card Diamonds Ace.
-	 * @return card Diamonds Ace
-	 */
-	public Card createCardDiamondsAce();
-
-	/**
-	 * Create card Hearts Seven.
-	 * @return card Hearts Seven
-	 */
-	public Card createCardHeartsSeven();
-
-	/**
-	 * Create card Hearts Eight.
-	 * @return card Hearts Eight
-	 */
-	public Card createCardHeartsEight();
-
-	/**
-	 * Create card Hearts Nine.
-	 * @return card Hearts Nine
-	 */
-	public Card createCardHeartsNine();
-
-	/**
-	 * Create card Hearts Ten.
-	 * @return card Hearts Ten
-	 */
-	public Card createCardHeartsTen();
-
-	/**
-	 * Create card Hearts Jack.
-	 * @return card Hearts Jack
-	 */
-	public Card createCardHeartsJack();
-
-	/**
-	 * Create card Hearts Queen.
-	 * @return card Hearts Queen
-	 */
-	public Card createCardHeartsQueen();
-
-	/**
-	 * Create card Hearts King.
-	 * @return card Hearts King
-	 */
-	public Card createCardHeartsKing();
-
-	/**
-	 * Create card Hearts Ace.
-	 * @return card Hearts Ace
-	 */
-	public Card createCardHeartsAce();
-
-	/**
-	 * Create card Spades Seven.
-	 * @return card Spades Seven
-	 */
-	public Card createCardSpadesSeven();
-
-	/**
-	 * Create card Spades Eight.
-	 * @return card Spades Eight
-	 */
-	public Card createCardSpadesEight();
-
-	/**
-	 * Create card Spades Nine.
-	 * @return card Spades Nine
-	 */
-	public Card createCardSpadesNine();
-
-	/**
-	 * Create card Spades Ten.
-	 * @return card Spades Ten
-	 */
-	public Card createCardSpadesTen();
-
-	/**
-	 * Create card Spades Jack.
-	 * @return card Spades Jack
-	 */
-	public Card createCardSpadesJack();
-
-	/**
-	 * Create card Spades Queen.
-	 * @return card Spades Queen
-	 */
-	public Card createCardSpadesQueen();
-
-	/**
-	 * Create card Spades King.
-	 * @return card Spades King
-	 */
-	public Card createCardSpadesKing();
-
-	/**
-	 * Create card Spades Ace.
-	 * @return card Spades Ace
-	 */
-	public Card createCardSpadesAce();
+	// /**
+	// * Create card Clubs Seven.
+	// * @return card Clubs Seven
+	// */
+	// public Card createCardClubsSeven();
+	//
+	// /**
+	// * Create card Clubs Eight.
+	// * @return card Clubs Eight
+	// */
+	// public Card createCardClubsEight();
+	//
+	// /**
+	// * Create card Clubs Nine.
+	// * @return card Clubs Nine
+	// */
+	// public Card createCardClubsNine();
+	//
+	// /**
+	// * Create card Clubs Ten.
+	// * @return card Clubs Ten
+	// */
+	// public Card createCardClubsTen();
+	//
+	// /**
+	// * Create card Clubs Jack.
+	// * @return card Clubs Jack
+	// */
+	// public Card createCardClubsJack();
+	//
+	// /**
+	// * Create card Clubs Queen.
+	// * @return card Clubs Queen
+	// */
+	// public Card createCardClubsQueen();
+	//
+	// /**
+	// * Create card Clubs King.
+	// * @return card Clubs King
+	// */
+	// public Card createCardClubsKing();
+	//
+	// /**
+	// * Create card Clubs Ace.
+	// * @return card Clubs Ace
+	// */
+	// public Card createCardClubsAce();
+	//
+	// /**
+	// * Create card Clubs Seven.
+	// * @return card Clubs Seven
+	// */
+	// public Card createCardDiamondsSeven();
+	//
+	// /**
+	// * Create card Diamonds Eight.
+	// * @return card Diamonds Eight
+	// */
+	// public Card createCardDiamondsEight();
+	//
+	// /**
+	// * Create card Diamonds Nine.
+	// * @return card Diamonds Nine
+	// */
+	// public Card createCardDiamondsNine();
+	//
+	// /**
+	// * Create card Diamonds Ten.
+	// * @return card Diamonds Ten
+	// */
+	// public Card createCardDiamondsTen();
+	//
+	// /**
+	// * Create card Diamonds Jack.
+	// * @return card Diamonds Jack
+	// */
+	// public Card createCardDiamondsJack();
+	//
+	// /**
+	// * Create card Diamonds Queen.
+	// * @return card Diamonds Queen
+	// */
+	// public Card createCardDiamondsQueen();
+	//
+	// /**
+	// * Create card Diamonds King.
+	// * @return card Diamonds King
+	// */
+	// public Card createCardDiamondsKing();
+	//
+	// /**
+	// * Create card Diamonds Ace.
+	// * @return card Diamonds Ace
+	// */
+	// public Card createCardDiamondsAce();
+	//
+	// /**
+	// * Create card Hearts Seven.
+	// * @return card Hearts Seven
+	// */
+	// public Card createCardHeartsSeven();
+	//
+	// /**
+	// * Create card Hearts Eight.
+	// * @return card Hearts Eight
+	// */
+	// public Card createCardHeartsEight();
+	//
+	// /**
+	// * Create card Hearts Nine.
+	// * @return card Hearts Nine
+	// */
+	// public Card createCardHeartsNine();
+	//
+	// /**
+	// * Create card Hearts Ten.
+	// * @return card Hearts Ten
+	// */
+	// public Card createCardHeartsTen();
+	//
+	// /**
+	// * Create card Hearts Jack.
+	// * @return card Hearts Jack
+	// */
+	// public Card createCardHeartsJack();
+	//
+	// /**
+	// * Create card Hearts Queen.
+	// * @return card Hearts Queen
+	// */
+	// public Card createCardHeartsQueen();
+	//
+	// /**
+	// * Create card Hearts King.
+	// * @return card Hearts King
+	// */
+	// public Card createCardHeartsKing();
+	//
+	// /**
+	// * Create card Hearts Ace.
+	// * @return card Hearts Ace
+	// */
+	// public Card createCardHeartsAce();
+	//
+	// /**
+	// * Create card Spades Seven.
+	// * @return card Spades Seven
+	// */
+	// public Card createCardSpadesSeven();
+	//
+	// /**
+	// * Create card Spades Eight.
+	// * @return card Spades Eight
+	// */
+	// public Card createCardSpadesEight();
+	//
+	// /**
+	// * Create card Spades Nine.
+	// * @return card Spades Nine
+	// */
+	// public Card createCardSpadesNine();
+	//
+	// /**
+	// * Create card Spades Ten.
+	// * @return card Spades Ten
+	// */
+	// public Card createCardSpadesTen();
+	//
+	// /**
+	// * Create card Spades Jack.
+	// * @return card Spades Jack
+	// */
+	// public Card createCardSpadesJack();
+	//
+	// /**
+	// * Create card Spades Queen.
+	// * @return card Spades Queen
+	// */
+	// public Card createCardSpadesQueen();
+	//
+	// /**
+	// * Create card Spades King.
+	// * @return card Spades King
+	// */
+	// public Card createCardSpadesKing();
+	//
+	// /**
+	// * Create card Spades Ace.
+	// * @return card Spades Ace
+	// */
+	// public Card createCardSpadesAce();
 
 }
